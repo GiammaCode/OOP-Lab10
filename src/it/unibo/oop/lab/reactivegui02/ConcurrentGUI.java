6fefed753ff344364f7e0b3d6076e977f02ddb53
Giammuzz <giammi53@hotmail.com>
2020-12-04 16:02:02 +0100

    first commit

---
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,88 @@
+package it.unibo.oop.lab.reactivegui02;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import it.unibo.oop.lab.reactivegui01.ConcurrentGUI;
+import it.unibo.oop.lab.reactivegui01.ConcurrentGUI.Agent;
+
+public class ConcurrentGUI extends JFrame {
+    
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+    private final JLabel display = new JLabel();
+    
+    public ConcurrentGUI() {
+        super();
+        
+        //set size
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        //make panel 
+        final JPanel panel = new JPanel();
+        this.setContentPane(panel);
+        panel.add(display);
+        panel.add(down);
+        panel.add(stop);
+        panel.add(up);
+        this.setVisible(true);
+        
+        //create a counter 
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+        
+        
+        
+    }
+    
+    private class Agent implements Runnable{
+        private volatile boolean stop;
+        private volatile int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    /*
+                     * All the operations on the GUI must be performed by the
+                     * Event-Dispatch Thread (EDT)!
+                     */
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(Integer.toString(Agent.this.counter)));
+                    this.counter++;
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    /*
+                     * This is just a stack trace print, in a real program there
+                     * should be some logging and decent error reporting
+                     */
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+    }
+    
+}
+
